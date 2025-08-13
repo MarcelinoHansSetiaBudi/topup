5e64249e1e5fd6268b6a41d60633f91ec237e330
Tjoa <100271617+MarcelinoHansSetiaBudi@users.noreply.github.com>
2025-08-13 21:26:28 +0700

    make layout navbar, display content slider, tab for game list at index

--- resources/views/hok.blade.php
+++ resources/views/hok.blade.php
@@ -1,49 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="dark">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>SB Store</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-                <img src="{{ asset('storage/logo/SB%20Store%20Logo.png') }}" alt="Logo"  width="35" height="35" class="rounded">
-                <span style="font-size: 14px; margin-left: 5px">Store</span>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">
-                            <i class="fas fa-home" style="font-size: 10pt">
-                                <span style="margin-left: 3px">Home</span>
-                            </i>
-                        </a>
-                        {{-- <i class="fa-regular fa-bolt-lightning"></i> --}}
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/flashsale">
-                            <i class="fas fa-bolt" style="font-size: 10pt;">
-                                <span style="margin-left: 3px">Flashsale</span>
-                            </i>
-                        </a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+@extends('layout.app')
 
+@section('content')
 
     <div class="container" style="padding-top: 10px; padding-bottom: 15px">
         <div class="row">
@@ -131,5 +88,4 @@
             </div>
         </div>
     </footer>
-</body>
-</html>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,140 +1,84 @@
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="dark">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
-    <title>SB Store</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-<style>
-    a:hover{
-        color: lightgray;
-    }
-</style>
+@extends('layout.app')
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-                <img src="{{ asset('storage/logo/SB%20Store%20Logo.png') }}" alt="Logo"  width="35" height="35" class="rounded">
-                <span style="font-size: 14px; margin-left: 5px">Store</span>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">
-                            <i class="fas fa-home" style="font-size: 10pt">
-                                <span style="margin-left: 3px">Home</span>
-                            </i>
-                        </a>
-                        {{-- <i class="fa-regular fa-bolt-lightning"></i> --}}
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/flashsale">
-                            <i class="fas fa-bolt" style="font-size: 10pt;">
-                                <span style="margin-left: 3px">Flashsale</span>
-                            </i>
-                        </a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+@section('content')
+
+
+<link rel="stylesheet" href="{{ asset('css/content.css') }}">
+
 <section style="background-color: #242a2e">
     <div class="container" >
-        <div class="slider-container" style="padding-bottom: 20px;">
+        <div class="slider-container mb-2">
             <button class="prev" onclick="moveSlide(-1)">&#10094;</button>
             
             <div class="slider">
                 <div class="slide">
                     <a href="/">
-                        <img src="{{ asset('Storage/Game/ML%20Chou.jpeg') }}" alt="Promo Image 1" style="border-radius: 50px">
+                        <img src="{{ asset('Storage/Game/ML%20Chou.jpeg') }}" alt="Promo Image 1">
                     </a>
                 </div>
                 <div class="slide">
                     <a href="/">
-                        <img src="{{ asset('Storage/Game/Pubg.jpg') }}" alt="Promo Image 2" style="border-radius: 50px">
+                        <img src="{{ asset('Storage/Game/Pubg.jpg') }}" alt="Promo Image 2">
                     </a>
                 </div>
                 <div class="slide">
                     <a href="/">
-                        <img src="{{ asset('Storage/Game/VALORANT.jpg') }}" alt="Promo Image 3" style="border-radius: 50px">
+                        <img src="{{ asset('Storage/Game/VALORANT.jpg') }}" alt="Promo Image 3">
                     </a>
                 </div>
-            </div>
-            
+            </div>        
             <button class="next" onclick="moveSlide(1)">&#10095;</button>
         </div>
     </div>
 </section>   
     
     {{-- Content --}}
-    <div class="container" style="padding-top: 10px">
+    <div class="container" style="padding-top:2cm">
         <div class="row">
-            <div class="col fw-bold"><img src="{{ asset('Storage/Logo/fire.jpg') }}" alt="fire" width="20px"> POPULER SEKARANG! </div>
+            <div class="col fw-bold"><img src="{{ asset('Storage/Logo/fire.jpg') }}" alt="fire" width="20px"> TEMUKAN PRODUK FAVORIT </div>
         </div>
-        <div class="row" style="padding-bottom: 30px; font-size: 10pt">
-            <div class="col">
-                Berikut adalah beberapa product yang paling populer saat ini.
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4 text-center">
-                <a href="/ml"><img src="{{ asset('Storage/Game/ML%20Chou.jpeg') }}" alt="ML Logo" width="250px" style="border-radius: 8px"></a>
-            </div>
-            <div class="col-sm-4 text-center">
-                <a href="/pubg"><img src="{{ asset('Storage/Game/Pubg.jpeg') }}" alt="PUBG Logo" width="250px"></a>
-            </div>
-            <div class="col-sm-4 text-center">
-                <a href="/hok"><img src="{{ asset('Storage/Game/Hok.jpg') }}" alt="Hok Logo" width="250px"></a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4 text-center">
-                <a href="/ml" style="text-decoration: none; color: white;"><p>Mobile Legend</p></a>
-            </div>
-            <div class="col-sm-4 text-center">
-                <a href="/pubg" style="text-decoration: none; color: white;"><p>PUBG MOBILE</p></a>
-            </div>
-            <div class="col-sm-4 text-center">
-                <p>Honor of Kings</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4 text-center">
-                <img src="{{ asset('Storage/Game/VALORANT.jpg') }}" alt="Valo Logo" width="250px">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4 text-center">
-                <p>Valorant</p>
+    </div>
+
+{{-- Game Tabs --}}
+<div class="container mb-4">
+    <div class="row">
+        <div class="col text-center">
+            <div class="game-tabs">
+                <span class="tab-item active" data-category="mobile">Mobile Game</span>
+                <span class="tab-item" data-category="pc">PC Game</span>
+                {{-- <span class="tab-item" data-category="voucher">VOUCHER</span> --}}
             </div>
         </div>
     </div>
-    {{-- Akhir Content --}}
+</div>
 
-    {{-- Store Name Logo --}}
-    <section style="background: gray; color: white;">
-        <div class="container" style="padding-top: 20px;">
-            <div class="row">
-                <div class="col text-center" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
-                    <h1>Welcome to SB Store</h1>
-                </div>
-            </div>  
+
+{{-- Game List --}}
+<div class="container">
+    <div class="row game-category" data-category="mobile">
+        <div class="col-sm-4 text-center mb-4">
+            <a href="/ml"><img src="{{ asset('Storage/Game/ML%20Chou.jpeg') }}" alt="ML Logo" width="250px" style="border-radius: 8px"></a>
+            <p class="text-white mt-2">Mobile Legend</p>
         </div>
-    </section>
-    {{-- Akhir Store Name Logo --}}
-    
+        <div class="col-sm-4 text-center mb-4">
+            <a href="/pubg"><img src="{{ asset('Storage/Game/Pubg.jpeg') }}" alt="PUBG Logo" width="250px"></a>
+            <p class="text-white mt-2">PUBG MOBILE</p>
+        </div>
+        <div class="col-sm-4 text-center mb-4">
+            <a href="/hok"><img src="{{ asset('Storage/Game/Hok.jpg') }}" alt="Hok Logo" width="250px"></a>
+            <p class="text-white mt-2">Honor of Kings</p>
+        </div>
+    </div>
+
+    <div class="row game-category" data-category="pc" style="display: none;">
+        <div class="col-sm-4 text-center mb-4">
+            <img src="{{ asset('Storage/Game/VALORANT.jpg') }}" alt="Valo Logo" width="250px">
+            <p class="text-white mt-2">Valorant</p>
+        </div>
+        {{-- Tambahkan game PC lainnya di sini jika ada --}}
+    </div>
+</div>
+
     {{-- Logo Kecil --}}
     <div class="container" style="margin-top: 10px; padding-top: 10px">
         <div class="row">
@@ -188,8 +132,8 @@
     {{-- Akhir Penjelasan Store --}}
 
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> --}}
-</body>
-</html>
+
+@endsection
 
 <script>
 let currentSlide = 0;
@@ -198,26 +142,50 @@ function moveSlide(direction) {
     const slides = document.querySelectorAll('.slide');
     const totalSlides = slides.length;
     const slider = document.querySelector('.slider');
-    
-    // Update currentSlide
+
     currentSlide += direction;
 
-    // Jika melebihi batas, kembali ke slide pertama atau terakhir
     if (currentSlide >= totalSlides) {
         currentSlide = 0;
     } else if (currentSlide < 0) {
         currentSlide = totalSlides - 1;
     }
 
-    // Geser slider dengan transform
-    const slideWidth = slides[0].clientWidth; // Dapatkan lebar slide
-    slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`; // Geser ke slide yang diinginkan
+    const slideWidth = slides[0].clientWidth;
+    slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
 
-// Set slide pertama sebagai aktif saat pertama kali load
 document.addEventListener('DOMContentLoaded', () => {
-    moveSlide(0); // Set slide awal
+    moveSlide(0);
 });
 
+ document.addEventListener('DOMContentLoaded', () => {
+        const tabs = document.querySelectorAll('.tab-item');
+        const categories = document.querySelectorAll('.game-category');
+
+        tabs.forEach(tab => {
+            tab.addEventListener('click', () => {
+                const selected = tab.getAttribute('data-category');
+
+                // Toggle active tab class
+                tabs.forEach(t => t.classList.remove('active'));
+                tab.classList.add('active');
+
+                // Show/Hide category content
+                categories.forEach(cat => {
+                    if (cat.getAttribute('data-category') === selected) {
+                        cat.style.display = 'flex';
+                    } else {
+                        cat.style.display = 'none';
+                    }
+                });
+            });
+        });
+
+        // Default: show mobile game
+        categories.forEach(cat => {
+            cat.style.display = cat.getAttribute('data-category') === 'mobile' ? 'flex' : 'none';
+        });
+    });
 
 </script>
